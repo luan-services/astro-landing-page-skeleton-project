3a706a8e7315ec033cacf6f1cee5bfc91dbeee6f
luan-services <luandotservices@gmail.com>
2025-09-16 20:13:56 -0300

    feat: add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  site: 'https://www.url-do-website.com.br',
+
+  integrations: [sitemap()]
 });
